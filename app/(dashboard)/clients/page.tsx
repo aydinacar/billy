7f4bd4a5f78446dbbaf157e934814f2988c62826
aydinacar <aydinacar@192.168.1.9>
2026-04-23 17:34:03 +0300

    feat: create table

--- app/(dashboard)/clients/page.tsx
+++ app/(dashboard)/clients/page.tsx
@@ -1,8 +1,26 @@
-export default function ClientsPage() {
+import { Client, columns } from '@/components/clients/table/columns'
+import { DataTable } from '@/components/clients/table/data-table'
+async function getData(): Promise<Client[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: '728ed52f',
+      amount: 100,
+      status: 'pending',
+      email: 'm@example.com'
+    }
+    // ...
+  ]
+}
+export default async function ClientsPage() {
+  const data = await getData()
   return (
-    <div className="flex flex-col items-center justify-center h-full text-center gap-6">
+    <div className="flex flex-col gap-6">
       <h1 className="text-4xl font-bold">Clients</h1>
-      <p className="text-lg text-muted-foreground max-w-md">Manage your clients and their invoices.</p>
+      <DataTable
+        columns={columns}
+        data={data}
+      />
     </div>
   )
 }
